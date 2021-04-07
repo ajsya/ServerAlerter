44a755040fdf8e48b10ce5784b61f3cb5a9b6dbf
ajsya <52182942+ajsya@users.noreply.github.com>
2021-04-07 11:14:47 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,40 +5,44 @@ import requests
 from mcstatus import MinecraftServer
 from pynput.mouse import Button, Controller
 
-mouse = Controller()
+# Settings
+SendDiscordMessage = False #Send a discord message when server open (True/False)
+DiscordWebhookURL = "" #Set a webhook url for the discord message to be sent to. Leave blank if SendDiscordMessage is set to False
+serverInput = input("Enter Server IP!")
+#serverInput = "" #OPTIONAL Enter a predefined server here and delete the line above
 
-#serverInput = input("Enter Server IP!")
-serverInput = "stoneworks.mcserv.fun"
+mouse = Controller()
 server = MinecraftServer.lookup(serverInput)
 
 status = server.status()
-# print("The server has {0} players and replied in {1} ms".format(status.players.online, status.latency))
 playerCount = status.players.online
+maxPlayerCount = status.players.max #EDIT LINE HERE TO MAXIUMUM PLAYER COUNT ROD
 print(playerCount)
 
 while True:
     status = server.status()
     playerCount = status.players.online
     # print("The server has {0} players and replied in {1} ms".format(status.players.online, status.latency))
-    if playerCount < 100:
+    if playerCount < maxPlayerCount:
         print('Server Open! There are only {0} player(s) online!'.format(status.players.online))
         mouse.click(Button.right, 1)
-        url = "https://discord.com/api/webhooks/829110060834881586/lm0SXZHSg09kf2YKjlbwp3w-1RwvOLyOmOTpx-tJu93hx-oDz1bR-ey2yNTBFPrOZxKe"
-
-        data = {
-            "content" : 'Server Open! There are only {0} player(s) online! ||@here||'.format(status.players.online),
-            "username" : serverInput
-        }
-
-        result = requests.post(url, json = data)
-
-        try:
-            result.raise_for_status()
-        except requests.exceptions.HTTPError as err:
-            print(err)
-        else:
-            print("Message sent successfully, code {}.".format(result.status_code))
+        if SendDiscordMessage = True:
+            url = DiscordWebhookURL
+            
+            data = {
+                "content" : 'Server Open! There are only {0} player(s) online! ||@here||'.format(status.players.online),
+                "username" : serverInput
+            }
+            
+            result = requests.post(url, json = data)
+            
+            try:
+                result.raise_for_status()
+            except requests.exceptions.HTTPError as err:
+                print(err)
+            else:
+                print("Message sent successfully, code {}.".format(result.status_code))
     else:
-        print('Server full currently with {0} players!'.format(status.players.online))
+        print('Server currently full with {0} players online!'.format(status.players.online))
     
-    time.sleep(0.1)
+    time.sleep(0.1) # Set the time interval between each request (in seconds)
